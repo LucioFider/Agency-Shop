56359254dedbb09cb8d2ad8d484ebd193c1e062d
LucioFider <fidelislucio404@gmail.com>
2023-02-26 15:15:32 +1000

    4th-commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="cards">
+      <div className="card bkg2-bg">
+        <span className="h3 accent3 bkg2-bg">1</span>
+        <h3>Build</h3>
+        <p>Build a string foundation of technique and theory.</p>
+      </div>
+      <div className="card bkg2-bg">
+        <span className="h3 accent3 bkg2-bg">2</span>
+        <h3>Develop</h3>
+        <p>Develop expressive, musical playing.</p>
+      </div>
+      <div className="card bkg2-bg">
+        <span className="h3 accent3 bkg2-bg">3</span>
+        <h3>Encourage</h3>
+        <p>Encourage musical potential of every student.</p>
+      </div>
+      <div className="card bkg2-bg">
+        <span className="h3 accent3 bkg2-bg">4</span>
+        <h3>Instill</h3>
+        <p>Instill a love for music and a joy for learning.</p>
+      </div>
+      <div className="card bkg2-bg">
+        <span className="h3 accent3 bkg2-bg">5</span>
+        <h3>Invest</h3>
+        <p>Invest in each student as an individual.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,149 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div class="blob-container" aria-hidden="true">
+      <svg
+        width="1108"
+        height="956"
+        viewBox="0 0 1108 956"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="blob"
+        alt="Blob"
+      >
+        <path
+          d="M1334.54 554.449C1263.07 768.899 1029.31 968.824 815.621 954.671C603.039 940.201 411.317 710.23 248.55 493.507C86.8901 276.467 -45.8155 72.6748 15.4535 -47.8575C75.2985 -169.179 330.225 -207.557 509.074 -248.066C689.029 -288.892 794.014 -332.166 899.526 -310.939C1005.35 -288.606 1111.71 -201.772 1211.8 -44.8973C1311.09 113.401 1404.91 340.317 1334.54 554.449Z"
+          class="accent1"
+        />
+      </svg>
+      <svg
+        width="871"
+        height="672"
+        viewBox="0 0 871 672"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="symbols"
+        alt="Symbols"
+      >
+        <path
+          d="M857.938 418.375C860.595 418.375 862.75 416.22 862.75 413.562C862.75 410.905 860.595 408.75 857.938 408.75C855.28 408.75 853.125 410.905 853.125 413.562C853.125 416.22 855.28 418.375 857.938 418.375Z"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M835.938 423.875C838.595 423.875 840.75 421.72 840.75 419.062C840.75 416.405 838.595 414.25 835.938 414.25C833.28 414.25 831.125 416.405 831.125 419.062C831.125 421.72 833.28 423.875 835.938 423.875Z"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M862.75 397.75L840.75 403.25"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M840.75 419.062V395L862.75 389.5V413.562"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M675.125 666.5C678.922 666.5 682 663.422 682 659.625C682 655.828 678.922 652.75 675.125 652.75C671.328 652.75 668.25 655.828 668.25 659.625C668.25 663.422 671.328 666.5 675.125 666.5Z"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M682 659.625V634.875L695.75 639V647.25L682 643.125"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M284.125 38.5C287.922 38.5 291 35.422 291 31.625C291 27.828 287.922 24.75 284.125 24.75C280.328 24.75 277.25 27.828 277.25 31.625C277.25 35.422 280.328 38.5 284.125 38.5Z"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M291 31.625V6.875L304.75 11V19.25L291 15.125"
+          class="bkg2--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M167.938 235.375C170.595 235.375 172.75 233.22 172.75 230.562C172.75 227.905 170.595 225.75 167.938 225.75C165.28 225.75 163.125 227.905 163.125 230.562C163.125 233.22 165.28 235.375 167.938 235.375Z"
+          class="accent1--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M145.938 240.875C148.595 240.875 150.75 238.72 150.75 236.062C150.75 233.405 148.595 231.25 145.938 231.25C143.28 231.25 141.125 233.405 141.125 236.062C141.125 238.72 143.28 240.875 145.938 240.875Z"
+          class="accent1--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M172.75 214.75L150.75 220.25"
+          class="accent1--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M150.75 236.062V212L172.75 206.5V230.562"
+          class="accent1--stroke"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M768.806 236.933L776.673 258.546"
+          class="bkg2--stroke"
+          stroke-width="3"
+        />
+        <path
+          d="M761.933 251.673L783.546 243.806"
+          class="bkg2--stroke"
+          stroke-width="3"
+        />
+        <path
+          d="M10.8065 75.9332L18.6729 97.5462"
+          class="accent1--stroke"
+          stroke-width="3"
+        />
+        <path
+          d="M3.93323 90.6729L25.5462 82.8064"
+          class="accent1--stroke"
+          stroke-width="3"
+        />
+      </svg>
+      <button class="theme" role="switch" aria-label="light/dark mode"></button>
+      <audio
+        src="audio/light-on.mp3"
+        class="theme-audio--light-on"
+        data-attribute="adapted from user 160033 file on freesound.org https://freesound.org/people/160033/sounds/366184/user CC BY-NC 3.0"
+      ></audio>
+      <audio
+        src="audio/light-off.mp3"
+        class="theme-audio--light-off"
+        data-attribute="adapted from user 160033 file on freesound.org https://freesound.org/people/160033/sounds/366184/user CC BY-NC 3.0"
+      ></audio>
+    </div>
+  );
 };
 
 export default Nav;
